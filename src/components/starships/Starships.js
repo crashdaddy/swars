8699f9218675de92e0d4885d1e1f4157c40b3b84
crashdaddy <kennethscoggins@mail.com>
2020-07-07 17:38:57 -0500

    added infinite scroll to starships

--- src/components/starships/Starships.js
+++ src/components/starships/Starships.js
@@ -1,59 +1,77 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
 
 
 class Starships extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
+            nextPage: "https://swapi.dev/api/starships/",
             starships: [],
             show: false
         }
     }
-    
+
     componentDidMount = () => {
+        window.addEventListener('scroll', this.infiniteScroll);
         this.getData();
     }
 
-getData = () => {
-    let starshipsURL = "https://swapi.dev/api/starships/";
-
-    this.fetchAPI(starshipsURL).then(data =>{
-        console.log(data)
-        this.setState({
-            starships: data,
-            show: true
+    getData = () => {
+        
+        this.fetchAPI(this.state.nextPage).then(data => {
+            console.log(data)
+            this.setState({
+                nextPage: data.next,
+                starships: [...this.state.starships,...data.results],
+                show: true
+            })
         })
-    })
-}
+    }
 
+    infiniteScroll = () => {
+        // End of the document reached?
+        if (window.innerHeight + document.documentElement.scrollTop
+        === document.documentElement.offsetHeight){
+         
+           this.getData();           
+        }
+    }
 
-fetchAPI = async (url) => {
+    fetchAPI = async (url) => {
 
-  let apiURL = url;
-  try {
-     const resp = await fetch(apiURL)
-     return resp.json()
-   } catch (err) {
-        console.log(err)
-     }
-}
+        let apiURL = url;
+        try {
+            const resp = await fetch(apiURL)
+            return resp.json()
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
 
     render() {
-        let starshipList = this.state.starships.results;
-        return(
+        let starshipList = this.state.starships;
+        let shadowStyle = {
+            WebkitBoxShadow: '5px 5px 5px 0px rgba(255, 255, 255, 0.25)',
+            MozBoxShadow: '5px 5px 5px 0px rgba(255, 255, 255, 0.25)',
+            boxShadow: '5px 5px 5px 0px rgba(255, 255, 255, 0.25)',
+            textAlign: 'center', width: '300px', 
+            margin: '15px', backgroundColor: 'lightgrey',color:'darkgrey',
+            position: 'relative', height: '200px'
+        }
+        return (
             <div>
                 {starshipList ?
-                <div style={{display:'flex',flexFlow:'row wrap',justifyContent:'space-around'}}>
-                {starshipList.map(starship => 
-                    <Paper elevation={3} style={{width:'225px',margin:'40px'}}>{starship.name}
-                    <img src={`${starship.url.replace('swapi.dev/api','crazyhappyfuntime.com/swimg')}1.jpg`} style={{maxWidth:'100%'}} />
-                    </Paper>
-                    )}
-                </div>
-                :
-                <div>No data</div>
+                    <div style={{ display: 'flex', flexFlow: 'row wrap', justifyContent: 'space-around' }}>
+                        {starshipList.map((starship, idx) =>
+                            <Paper key={idx} elevation={3} style={shadowStyle}>{starship.name}
+                                <img src={`${starship.url.replace('swapi.dev/api', 'crazyhappyfuntime.com/swimg')}1.jpg`} style={{ width: '100%', height: '126px', position: 'absolute', bottom: '2px', left: '0px' }} />
+                            </Paper>
+                        )}
+                    </div>
+                    :
+                    <div>No data</div>
                 }
             </div>
         )
